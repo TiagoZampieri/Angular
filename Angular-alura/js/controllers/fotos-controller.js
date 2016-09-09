26b487d49424c7dc591955f9c01e47f99560cb09
Tiago Zampieri <tzampieri7@gmail.com>
2016-09-09 13:58:38 -0300

    Add files via upload

---
+++ Angular-alura/js/controllers/fotos-controller.js
@@ -0,0 +1,18 @@
+angular.module('alurapic').controller('FotosController', function($scope) {
+	
+	$scope.fotos = [
+		{
+			titulo : 'Angular',
+			url: 'https://cms-assets.tutsplus.com/uploads/users/12/posts/14047/preview_image/angular-js.png'
+		},
+		{
+			titulo : 'Angular 2',
+			url: 'https://cms-assets.tutsplus.com/uploads/users/12/posts/14047/preview_image/angular-js.png'
+		},
+		{
+			titulo : 'Angular 3',
+			url: 'https://cms-assets.tutsplus.com/uploads/users/12/posts/14047/preview_image/angular-js.png'
+		},
+	];
+
+}); 
